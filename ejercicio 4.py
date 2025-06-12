eccc7cb806c94b9a7cac4a44bf8ce7d7b03ad368
dantec123 <dante.corzo.081@alu.tecnica29de6.edu.ar>
2025-06-12 07:31:19 -0300

    Crear ejercicio 4.py

---
+++ ejercicio 4.py
@@ -0,0 +1,37 @@
+class empleado:
+    def calcular_salario(self):
+        pass  
+
+
+class empleadoporHora(empleado):
+    def __init__(self, horas_trabajadas, pago_por_hora):
+        self.horas = horas_trabajadas
+        self.pago = pago_por_hora
+
+    def calcular_salario(self):
+        return self.horas * self.pago
+
+
+class empleadoFijo(empleado):
+    def __init__(self, salario_mensual):
+        self.salario = salario_mensual
+
+    def calcular_salario(self):
+        return self.salario
+
+
+if __name__ == "__main__":
+    empleados = [
+        empleadoporHora(35, 1430),
+        empleadoFijo(20000),
+        empleadoporHora(15, 1200),
+        empleadoFijo(84500)
+    ]
+
+    total = 0
+    for emp in empleados:
+        salario = emp.calcular_salario()
+        print(f"Salario: ${salario}")
+        total += salario
+
+    print(f"\nSalario total a pagar: ${total}")
